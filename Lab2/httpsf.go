be9e2b7a9159617c12c9179f4cbd59bbbff154b6
Mohamed <mo7_abd@hotmail.com>
2017-11-05 14:33:00 -0500

    Created Parse request function

--- Lab2/httpsf.go
+++ Lab2/httpsf.go
@@ -41,6 +41,13 @@ var v bool
 var p int
 var d string
 
+// Prints logs when in verbose mode
+func verbose(log string) {
+	if v {
+		fmt.Println(log)
+	}
+}
+
 func main() {
 
 	// cli tool usage info message to be displayed
@@ -66,14 +73,15 @@ func main() {
 		}
 	}
 
+	// if help flag is present
 	if help {
 		fmt.Println(helpStr)
 	} else {
+
 		port := fmt.Sprintf(":%d", p)
 
-		if v {
-			fmt.Printf("Listening to Localhost%s\n", port)
-		}
+		verbose(fmt.Sprintf("Listening to Localhost%s", port))
+
 		// listen on all interfaces
 		listener, _ := net.Listen("tcp", port)
 		for {
@@ -88,6 +96,7 @@ func main() {
 
 }
 
+// Handles server connections
 func handleConn(conn net.Conn) {
 	defer conn.Close()
 	fmt.Printf("Request recieved from %v\n", conn.RemoteAddr())
@@ -98,124 +107,142 @@ func handleConn(conn net.Conn) {
 		fmt.Fprintf(os.Stderr, "read error %v\n", re)
 	}
 
-	if v {
-		fmt.Println("\n", string(buf))
-	}
+	verbose(fmt.Sprintln("\n", string(buf)))
 
+	// Parsing request into Request struct
 	var request Request
 	if n > 0 {
-		var lines []string
-		lines = strings.Split(string(buf), "\r\n")
-		head := strings.Split(lines[0], " ")
-
-		if len(head) > 2 {
-			u, _ := url.Parse(head[1])
-			headers := make(map[string]string)
-			var body string
-			for i := 1; i < len(lines); i += 2 {
-				if lines[i] != "" {
-					line := strings.Split(lines[i], ":")
-					headers[line[0]] = line[1]
-				} else {
-					if i < len(lines) {
-						body = lines[i+1]
-					}
-					break
-				}
-				request = Request{head[0], u, head[2], headers, body}
-			}
-
-		}
-
-		var response string
-
-		if request.method == "GET" {
-			var a []File
-			if request.URL.Path == "/" {
-				files, err := ioutil.ReadDir("." + request.URL.Path)
-				if err != nil {
-					response += request.httpversion + " 404 OK \r\n"
-					log.Println(err)
-				}
-				response += request.httpversion + " 200 OK \r\n"
-				for _, f := range files {
-					a = append(a, File{f.Name(), ""})
-				}
-			} else {
-				response += request.httpversion + " 200 OK \r\n"
-				efile, err := ioutil.ReadFile("." + request.URL.Path)
-				fmt.Println(fmt.Sprintf("%s", efile))
-				a = append(a, File{"." + request.URL.Path, fmt.Sprintf("%s", efile)})
-				if err != nil {
-					response += request.httpversion + " 404 OK \r\n"
-					log.Println(err)
-				}
-			}
-
-			var body string
-			switch request.headers["accept"] {
-			case "application/json":
-				jsonData, _ := json.Marshal(a)
-				body = fmt.Sprintf("%s", jsonData)
-				fmt.Println(body)
-				break
-			case "text/html":
-				var htmlData string
-				for _, element := range a {
-					htmlData += fmt.Sprintf("<li>%s</li>\n", element.FileName)
-				}
-				body = fmt.Sprintf("<html>\n<body>\n<ul>\n%s</ul>\n</body>\n</html>\n", htmlData)
-				break
-			case "text/xml":
-				xmlData, _ := xml.Marshal(a)
-				body = fmt.Sprintf("%s", xmlData)
-				break
-			default:
-				for _, element := range a {
-					body = element.FileName + " \n" + element.content + "\n"
-				}
-				break
-			}
-			response += "Message Body:\n"
-			response += body + "\r\n"
-
-		}
-		if (request.method == "POST") && request.URL.Path != "/" {
-			// write the whole body at once
-			// get FileInfo structure describing file
-			_, err := os.Stat(d + request.URL.Path + ".txt")
-			if os.IsNotExist(err) {
-				fileHandle, _ := os.Create(d + request.URL.Path + ".txt")
-				writer := bufio.NewWriter(fileHandle)
-				fmt.Fprintln(writer, request.body)
-				writer.Flush()
-				fileHandle.Close()
-			} else {
-				if request.URL.Query()["overwrite"] != nil {
-					if request.URL.Query()["overwrite"][0] == "true" {
-						if v {
-							fmt.Println("Overwrite is true")
-						}
-						fileHandle, _ := os.Create(d + request.URL.Path + ".txt")
-						writer := bufio.NewWriter(fileHandle)
-						fmt.Fprintln(writer, request.body)
-						writer.Flush()
-						fileHandle.Close()
-					}
-				}
-
-			}
-
-			response += request.body
-
-		}
-		if v {
-			fmt.Println(response + "\r\n")
-		}
-		if _, we := conn.Write([]byte(response)); we != nil {
-			fmt.Fprintf(os.Stderr, "write error %v\n", we)
-		}
-		conn.Close()
+		request = parseRequest(buf)
+	} else {
+		verbose("Invalid Request")
+		return
 	}
 
+	var response string
+
+	// GET method
+	if request.method == "GET" {
+		var a []File
+		if request.URL.Path == "/" {
+			files, err := ioutil.ReadDir("." + request.URL.Path)
+			if err != nil {
+				response += request.httpversion + " 404 OK \r\n"
+				log.Println(err)
+			}
+			response += request.httpversion + " 200 OK \r\n"
+			for _, f := range files {
+				a = append(a, File{f.Name(), ""})
+			}
+		} else {
+			response += request.httpversion + " 200 OK \r\n"
+			efile, err := ioutil.ReadFile("." + request.URL.Path)
+			fmt.Println(fmt.Sprintf("%s", efile))
+			a = append(a, File{"." + request.URL.Path, fmt.Sprintf("%s", efile)})
+			if err != nil {
+				response += request.httpversion + " 404 OK \r\n"
+				log.Println(err)
+			}
+		}
+
+		var body string
+		switch request.headers["accept"] {
+		case "application/json":
+			jsonData, _ := json.Marshal(a)
+			body = fmt.Sprintf("%s", jsonData)
+			verbose(body)
+			break
+		case "text/html":
+			var htmlData string
+			for _, element := range a {
+				htmlData += fmt.Sprintf("<li>%s</li>\n", element.FileName)
+			}
+			body = fmt.Sprintf("<html>\n<body>\n<ul>\n%s</ul>\n</body>\n</html>\n", htmlData)
+			verbose(body)
+			break
+		case "text/xml":
+			xmlData, _ := xml.Marshal(a)
+			body = fmt.Sprintf("%s", xmlData)
+			verbose(body)
+			break
+		default:
+			for _, element := range a {
+				body += element.FileName + " \n" + element.content + "\n"
+			}
+			verbose(body)
+			break
+		}
+		response += "Message Body:\n"
+		response += body + "\r\n"
+
+	}
+	// POST method
+	if (request.method == "POST") && request.URL.Path != "/" {
+		// write the whole body at once
+		// get FileInfo structure describing file
+		_, err := os.Stat(d + request.URL.Path + ".txt")
+		if os.IsNotExist(err) {
+			fileHandle, _ := os.Create(d + request.URL.Path + ".txt")
+			writer := bufio.NewWriter(fileHandle)
+			fmt.Fprintln(writer, request.body)
+			writer.Flush()
+			fileHandle.Close()
+		} else {
+			if request.URL.Query()["overwrite"] != nil {
+				if request.URL.Query()["overwrite"][0] == "true" {
+
+					verbose("Overwrite is true")
+
+					fileHandle, _ := os.Create(d + request.URL.Path + ".txt")
+					writer := bufio.NewWriter(fileHandle)
+					fmt.Fprintln(writer, request.body)
+					writer.Flush()
+					fileHandle.Close()
+				}
+			}
+
+		}
+
+		response += request.body
+
+	}
+	verbose(fmt.Sprintln(response + "\r\n"))
+
+	if _, we := conn.Write([]byte(response)); we != nil {
+		fmt.Fprintf(os.Stderr, "write error %v\n", we)
+	}
+	conn.Close()
+
+}
+
+//Function to take buffer data and parse it into Request
+func parseRequest(buf []byte) (request Request) {
+	var lines []string
+	//Split each line
+	lines = strings.Split(string(buf), "\r\n")
+
+	// Split the first line with the request definition
+	head := strings.Split(lines[0], " ")
+
+	if len(head) > 2 {
+		u, _ := url.Parse(head[1])
+		headers := make(map[string]string)
+		var body string
+		for i := 1; i < len(lines); i += 2 {
+			if lines[i] != "" {
+				line := strings.Split(lines[i], ": ")
+				if len(line) > 1 {
+					headers[line[0]] = line[1]
+				}
+
+			} else {
+				if i < len(lines) {
+					body = lines[i+1]
+				}
+				break
+			}
+
+		}
+		request = Request{head[0], u, head[2], headers, body}
+	}
+	return
 }
